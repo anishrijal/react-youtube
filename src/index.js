9b42fa6127d78bbd8c497ad9b60b20871c35a4e8
Donovan Goldston <donovan.goldston@Ksquarelabs.com>
2017-01-03 13:51:16 -0600

    search bar done

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'React';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY= 'AIzaSyDWjxHJvmrP45RoodSPjeZ7KPxIwIDDZHo';
 
 const App = () => {
-  return <div>hi!</div>
+  return <div>
+  <SearchBar />
+  </div>
 }
 
 
