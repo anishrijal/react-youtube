3305bafe3d455b4f30735baf07fba64bb55e82a2
Donovan Goldston <donovan.goldston@Ksquarelabs.com>
2017-01-03 16:00:44 -0600

    video iframe works

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyDWjxHJvmrP45RoodSPjeZ7KPxIwIDDZHo';
 
 
@@ -16,7 +17,7 @@ class App extends Component {
         super(props);
 
         this.state = { videos: [] };
-        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+        YTSearch({key: API_KEY, term: 'star wars'}, (videos) => {
               this.setState({videos});//ES6; equals videos: videos;
         });
     }
@@ -24,6 +25,7 @@ class App extends Component {
         return (
         <div>
             <SearchBar />
+            <VideoDetail video ={this.state.videos[0]} />
             <VideoList videos={this.state.videos}/>
             </div>
           )
