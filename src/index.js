08f776c1c3da2fdad26151bee08b150f50ad565a
Donovan Goldston <donovan.goldston@Ksquarelabs.com>
2017-01-03 12:06:44 -0600

    starting to work with the search menu

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
-import React from 'react';
+import React from 'React';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY= 'AIzaSyDWjxHJvmrP45RoodSPjeZ7KPxIwIDDZHo';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const App = () => {
+  return <div>hi!</div>
+}
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+
+
+
+ReactDOM.render(<App />,
+  document.querySelector('.container'));
