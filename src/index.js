aacd4ed6997d8df7c850c92964b1ec14cf7e3f14
Donovan Goldston <donovan.goldston@Ksquarelabs.com>
2017-01-03 15:25:27 -0600

    list started

--- src/index.js
+++ src/index.js
@@ -1,18 +1,32 @@
-import React from 'React';
+import React, {
+    Component
+} from 'React';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+const API_KEY = 'AIzaSyDWjxHJvmrP45RoodSPjeZ7KPxIwIDDZHo';
 
-const API_KEY= 'AIzaSyDWjxHJvmrP45RoodSPjeZ7KPxIwIDDZHo';
 
-const App = () => {
-  return <div>
-  <SearchBar />
-  </div>
+
+
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+              this.setState({videos});//ES6; equals videos: videos;
+        });
+    }
+    render() {
+        return (
+        <div>
+            <SearchBar />
+            <VideoList videos={this.state.videos}/>
+            </div>
+          )
+    }
 }
-
-
-
-
-ReactDOM.render(<App />,
-  document.querySelector('.container'));
+ReactDOM.render( < App / > , document.querySelector('.container'));
